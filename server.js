38e8be022f167b86d11e35c9975cb7c433028be3
lizheng <v-lizheng@expediagroup.com>
2022-04-05 18:28:13 +0800

    prodcution enviroment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const {graphqlHTTP}=require('express-graphql');
 const cors=require('cors');
 const schema=require('./schema')
+const path=require('path')
 
 
 const app=express();
@@ -11,7 +12,11 @@ app.use('/graphql',graphqlHTTP({
     graphiql:true
 }));
 
+app.use(express.static('public'))
 
+app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,'public','index.html'))
+})
 const PORT=process.env.PORT||4000
 app.listen(PORT,()=>{
     console.log('4000端口正在启动')
