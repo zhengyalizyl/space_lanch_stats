cda58092178cb2f240b2f7d64071ab22589afa52
lizheng <v-lizheng@expediagroup.com>
2022-04-05 18:26:06 +0800

    prodcution enviroment

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 const client=new ApolloClient({
-  uri:'http://localhost:4000/graphql',
+  uri:'/graphql',
   cache:new InMemoryCache(),
   grapihql:true
 })
